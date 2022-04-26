b2e0f4d56c989d83aa5d439263b2da64386df33a
Salvatore Allegra <salallegra@gmail.com>
2022-04-26 15:17:45 -0700

    Add project files.

---
+++ Card.cs
@@ -0,0 +1,16 @@
+﻿namespace CardClassLibrary
+{
+    public class Card
+    {
+        private string Suit { get; }
+
+        private string Rank { get; }
+
+        public Card(string suit,string rank){
+            this.Suit = suit;
+            this.Rank = rank;
+        }
+
+        public override string ToString() => $"{Rank} of {Suit}";
+    }
+}
