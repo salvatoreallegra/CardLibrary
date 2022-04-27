defc3fe1e9248f4465f961badb0ce8d98acacb27
Salvatore Allegra <salallegra@gmail.com>
2022-04-26 17:34:30 -0700

    Deal out cards in top level program.cs, Make a hand class to deal out one hold'em hand.

---
+++ Hand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardClassLibrary
+{
+    public class Hand
+    {
+        Card[] holdEmHand = new Card[2];
+
+        public Hand()
+        {
+            Deck deck = new Deck();
+            deck.shuffle();
+            for(int i = 0; i < holdEmHand.Length; i++)
+            {
+                holdEmHand[i] = deck.dealCard();
+            }
+        }
+
+        public void dealHoldemHand()
+        {
+            foreach(Card card in holdEmHand)
+            {
+                Console.WriteLine(card.ToString());
+            }
+        }
+    }
+}
